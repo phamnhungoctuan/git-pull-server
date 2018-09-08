442f5889f1ae462873cf7b0a3746827401722241
phamnhungoctuan <phamnhungoctuan@gmail.com>
2018-09-09 00:09:32 +0700

    delete

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("TOKEN", "token-la-cai-veo-gi");                                       // The secret token to add as a GitHub or GitLab secret, or otherwise as https://www.example.com/?token=secret-token
-define("REMOTE_REPOSITORY", ""); // The SSH URL to your repository
-define("DIR", "/home/etc");                          // The path to your repostiroy; this must begin with a forward slash (/)
-define("BRANCH", "refs/heads/master");                                 // The branch route
-define("LOGFILE", "deploy.log");                                       // The name of the file you want to log to.
-define("GIT", "/usr/bin/git");                                         // The path to the git executable
-define("AFTER_PULL", "");                                              // A command to execute after successfully pulling
